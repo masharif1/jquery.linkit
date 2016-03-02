518df6c5cd8b334030b33a820964a27e57bef0db
Muhammad Amir Sharif <msharif4@gmail.com>
2016-03-02 16:54:54 +0100

    0.2.0 release

---
+++ jquery.linkit.js
@@ -0,0 +1,32 @@
+/*
+* name: Linkit
+* author: M. Amir Sharif
+* version: 0.1.0
+* license: MIT
+*/
+
+(function($) {
+    $.fn.linkit = function(options){
+        //Default Settings
+        var settings = $.extend ({
+            href   : null,
+            text   :null,
+            target : '_self'
+        }, options);
+        
+        // Validation
+            if(settings.href == null){
+                console.log('You need an herf Option for Linkit to work');
+                return this;
+            }
+        return this.each(function(){
+            var object = $(this);
+            if (settings.text == null){
+                settings.text = object.text();
+            }
+             object.wrap('<a target="'+settings.target+'" href="'+settings.href+'"></a>').text(settings.text);
+        });
+        
+        }
+    
+}(jQuery));
